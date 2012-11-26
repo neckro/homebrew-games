3aa3ff45fdd17d05b021ca060d452d22d088f857
Joseph C <neckro@gmail.com>
2012-11-26 16:16:36 -0600

    Remote caveat

--- stone-soup.rb
+++ stone-soup.rb
@@ -7,17 +7,6 @@ class StoneSoup < Formula
 
   depends_on 'xz' => :build
 
-  def caveats; <<-EOS.undent
-    If you upgraded from 0.7.2, your saved games are not compatible with 0.11.
-    Your 0.7.2 saved games are kept in:
-      #{HOMEBREW_PREFIX}/Cellar/stone-soup/0.7.2/saves
-    Version 0.11's save files are kept in:
-      ~/Library/Application Support/Dungeon Crawl Stone Soup/saves
-    To revert to the older version:
-      brew switch stone-soup 0.7.2
-    EOS
-  end
-
   def install
     cd "source" do
       system "make", "prefix=#{prefix}", "DATADIR=data/", "install"
